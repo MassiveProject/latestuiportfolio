773894a8b07f7b3cd71218047c7d814ca7b78878
william <william@eskalink.com>
2025-05-28 18:42:00 +0700

    update lagi

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1TTMme9YFt0QwWhpdY2dlNb1dsBbJGRX7/view?usp=sharing"
+                href="https://drive.google.com/file/d/1-XnwRGRAeV6M9aI-zL4y9p_TzLmbNICJ/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 {" "}
